6a54b6e725c1c8adad9bb55a6572624ea18c60a5
Aaron Grider <aaron.grider@gmail.com>
2015-03-04 10:39:11 -0800

    Added makeButton method

--- app/src/main/java/edu/spu/teamroot/voicecloud/MainActivity.java
+++ app/src/main/java/edu/spu/teamroot/voicecloud/MainActivity.java
@@ -1,6 +1,9 @@
 package edu.spu.teamroot.voicecloud;
 
+import android.animation.ObjectAnimator;
 import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.TypedValue;
@@ -10,6 +13,7 @@ import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
+import android.view.animation.BounceInterpolator;
 import android.widget.HorizontalScrollView;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
@@ -18,6 +22,8 @@ import android.widget.Toast;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.HorizontalScrollView;
+
+import java.util.Random;
 
 public class MainActivity extends ActionBarActivity {
     static Toast lastToast;
@@ -28,6 +34,12 @@ public class MainActivity extends ActionBarActivity {
 
     TwoDScrollView scrollView;
     RelativeLayout rl;
+
+    public String[] accentColors = {
+            "#20a760", // green
+            "#3d83f7", // blue
+            "#dc4339", // red
+            "#ffe233"};// yellow
 
     private static float MIN_ZOOM = 1f;
     private static float MAX_ZOOM = 5f;
@@ -73,29 +85,10 @@ public class MainActivity extends ActionBarActivity {
         // Add the RelativeLayout element to the ScrollView
         scrollView.addView(rl);
 
-        // Add a bunch of buttons for testing
-        for (int i = 0; i < 40; i++)
-        {
-            Button button = new Button(this);
-            button.setTextSize(TypedValue.COMPLEX_UNIT_SP, 12 + (Math.round(Math.random() * 100) % 50));
-            button.setText("Button" + Integer.toString(i));
+        makeButton("Hello", 60, 20, 300);
 
-            button.setOnTouchListener(new View.OnTouchListener() {
-                @Override
-                public boolean onTouch(View v, MotionEvent event) {
-                    if (lastToast != null) lastToast.cancel();
-                    lastToast = Toast.makeText(MainActivity.this, "Clicked " + ((Button)v).getText().toString(), Toast.LENGTH_SHORT);
-                    lastToast.show();
-                    return false;
-                }
-            });
-
-            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-            params.leftMargin = toPx(20 + (Math.round(Math.random() * 100)) * 10);
-            params.topMargin = toPx(20 + i * 55);
-
-            rl.addView(button, params);
-        }
+        // Create random buttons
+        //randomButtons();
     }
 
 
@@ -127,5 +120,83 @@ public class MainActivity extends ActionBarActivity {
 
     private int toDp(float px) {
         return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PX, px, getResources().getDisplayMetrics()));
+    }
+
+    public void randomButtons() {
+
+        // Add a bunch of buttons for testing
+        for (int i = 0; i < 40; i++)
+        {
+            Button button = new Button(this);
+            button.setTextSize(TypedValue.COMPLEX_UNIT_SP, 12 + (Math.round(Math.random() * 100) % 50));
+            button.setText("Button" + Integer.toString(i));
+
+            // Create new color filter and set button background color
+            PorterDuffColorFilter filter = new PorterDuffColorFilter(getRandomAccentColor(), PorterDuff.Mode.SRC_ATOP);
+            button.getBackground().setColorFilter(filter);
+
+
+            button.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    if (lastToast != null) lastToast.cancel();
+                    lastToast = Toast.makeText(MainActivity.this, "Clicked " + ((Button)v).getText().toString(), Toast.LENGTH_SHORT);
+                    lastToast.show();
+                    return false;
+                }
+            });
+
+            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+            params.leftMargin = toPx(20 + (Math.round(Math.random() * 100)) * 10);
+            params.topMargin = toPx(20 + i * 55);
+
+            rl.addView(button, params);
+        }
+
+    }
+
+    public void makeButton(String name, int size, int x, int y) {
+
+        Button button = new Button(this);
+        button.setTextSize(size);
+        button.setText(name);
+
+        // Create new color filter and set button background color
+        PorterDuffColorFilter filter = new PorterDuffColorFilter(getRandomAccentColor(), PorterDuff.Mode.SRC_ATOP);
+        button.getBackground().setColorFilter(filter);
+
+        button.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (lastToast != null) lastToast.cancel();
+                lastToast = Toast.makeText(MainActivity.this, "Clicked " + ((Button)v).getText().toString(), Toast.LENGTH_SHORT);
+                lastToast.show();
+                return false;
+            }
+        });
+
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        params.leftMargin = toPx(x);
+        params.topMargin = toPx(y);
+
+        // Add animation
+
+        rl.addView(button, params);
+
+    }
+
+    public int getRandomAccentColor() {
+
+        String color = "";
+
+        // Randomly select a fact
+        Random randomGenerator = new Random();  // Construct a new Random number generator
+        int randomNumber = randomGenerator.nextInt(accentColors.length);
+
+        color = accentColors[randomNumber];
+        int colorAsInt = Color.parseColor(color);
+
+        return colorAsInt;
+
     }
 }
